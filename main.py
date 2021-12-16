8beed53a9b2efdc89aa171bb8fefa06454cf6c7a
Hyeongseok Jeon <521hsgugu@kaist.ac.kr>
2021-12-16 10:04:29 +0900

    asd

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from math import cos,sin,sqrt,pow,atan2,pi
 import time
 import threading
 import os,json
+from stanley import StanleyController
 
 # path = os.path.dirname(os.path.abspath( __file__ ))
 path = os.getcwd()
@@ -28,19 +29,26 @@ TL_get = udp_parser(user_ip, TL_get_port,'get_traffic')
 TL_set = udp_sender(host_ip, TL_set_port,'set_traffic')
 ego_ctrl = udp_sender(host_ip, ego_ctrl_port, 'ctrl_cmd')
 
-ego_parse = dict()
+path_tracker = StanleyController()
+ego_status =dict()
 sur_parse = dict()
 i = 0
 while True:
     i = i + 0.01
     sur_data = sur.get_data()
     ego_data = ego.get_data()
-    TL_idx = TL_get.get_data()[0]
-    TL_set.send_data([TL_get.get_data()[0], 4])
-    ego_parse['vel_mps'] = ego_data[2] /3.6
-    ego_parse['x'] = ego_data[12]
-    ego_parse['y'] = ego_data[13]
-    ego_parse['heading'] = ego_data[17]
+    TL_idx = TL_get.get_data()
+    TL_set.send_data([TL_idx[0], 4])
+    '''여기 아래서 부터는 계속 작업중인 부분이니 고려안하셔도 됩니다'''
+    ego_status['vel'] = ego_data[2]
+    ego_status['x'] = ego_data[12]
+    ego_status['y'] = ego_data[13]
+    ego_status['heading'] = ego_data[17]
+
+    path_tracker.set_ego_status(ego_status)
+    self.pure_pursuit.getPath(local_path)
+    self.pure_pursuit.getEgoStatus(position_x, position_y, position_z, velocity, heading)
+    steering_angle = self.pure_pursuit.steering_angle()
 
     ctrl_mode = 2  # 2 = AutoMode / 1 = KeyBoard
     Gear = 4  # 4 1 : (P / parking ) 2 (R / reverse) 3 (N / Neutral)  4 : (D / Drive) 5 : (L)
@@ -49,7 +57,7 @@ while True:
     acceleration = 0  # cmd_type이 3일때 원하는 가속도를 넣어준다.
     accel = 0
     brake = 0
-    steering_angle = 20 * np.sin(2*np.pi*0.1*i)
+    steering_angle = np.sin(2*np.pi*i)
     print(steering_angle)
 
     ego_ctrl.send_data([ctrl_mode, Gear, cmd_type, send_velocity, acceleration, accel, brake, steering_angle])
---
+++ stanley.py
@@ -0,0 +1,56 @@
+import numpy as np
+from math import cos,sin, radians
+
+class StanleyController:
+    def __init__(self, k=0.5, wheelbase=2.8, polynomial_order=3):
+        # control agin
+        self.k = k
+        self.wheelbase = wheelbase
+        self.polynomial_order = polynomial_order
+
+        self.vehicle_position = np.matrix([[0.0], [0.0]])
+        self.vehicle_angle = 0.0
+        self.vehicle_velocity = 0.0
+
+        self.path = np.matrix([[0.0], [0.0]])
+
+    def set_ego_status(self, status_msg):
+        '''
+        ego_status['vel'] = ego_data[2]
+        ego_status['x'] = ego_data[12]
+        ego_status['y'] = ego_data[13]
+        ego_status['heading'] = ego_data[17]
+
+        '''
+        self.current_vel = status_msg['vel'] / 3.6  # m/s
+        self.vehicle_angle = radians(status_msg['heading'])
+        # rear to front wheel position
+        self.vehicle_position[0] = status_msg['x'] + self.wheelbase * np.cos(self.vehicle_angle)
+        self.vehicle_position[1] = status_msg['y'] + self.wheelbase * np.sin(self.vehicle_angle)
+
+    def set_path(self, path_msg):
+        x = [poses.pose.position.x for poses in path_msg.poses]
+        y = [poses.pose.position.y for poses in path_msg.poses]
+        self.path = np.matrix([x, y])
+
+    def steering_angle(self):
+        R = np.matrix(
+            [[cos(-self.vehicle_angle), -sin(-self.vehicle_angle)],
+             [sin(-self.vehicle_angle), cos(-self.vehicle_angle)]]
+        )
+        local_path = self.path - np.matlib.repmat(self.vehicle_position, 1, self.path.shape[1])
+        rotated_local_path = R.dot(local_path)
+
+        path_coefficients = np.polyfit(
+            np.squeeze(np.array(rotated_local_path[0, :])),
+            np.squeeze(np.array(rotated_local_path[1, :])),
+            self.polynomial_order,
+        )
+        path_coefficients_derivative = np.polyder(path_coefficients)
+
+        lateral_offset = np.polyval(path_coefficients, 0)
+        theta_distance = np.arctan2(self.k * lateral_offset, self.current_vel)
+        theta_heading = np.arctan(np.polyval(path_coefficients_derivative, 0))
+
+        theta = theta_distance + theta_heading
+        return theta
